31287b8c060b65a4f042c909a16da68dec56d7ee
zackmorgs <zackmorgenthaler@gmail.com>
2025-07-03 14:20:08 -0500

    block theme starting parts

--- functions.php
+++
@@ -1,6 +0,0 @@
-<?php
-function zackmorgenthaler_enqueue_styles(): void {
-    wp_enqueue_style('main-style', get_stylesheet_uri());
-}
-
-add_action('wp_enqueue_scripts', 'zackmorgenthaler_enqueue_styles');

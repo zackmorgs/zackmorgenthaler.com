ba6f06cad1daf14df0c3ea742ae55900ca547c16
zackmorgs <zackmorgenthaler@gmail.com>
2025-07-03 11:32:39 -0500

    removed old code

--- footer.php
+++ footer.php
@@ -1,38 +1,8 @@
 <?php wp_footer(); ?>
 <script src="<?php echo get_template_directory_uri(); ?>/assets/script.min.js"></script>
 <footer id="footer_main">
-    <div class="footer-social">
-        <ul class="social-links">
-            <li>
-                <a href="https://www.facebook.com/STILLSTAYER/" target="_blank" rel="noopener noreferrer">
-                    <img src="<?php echo get_template_directory_uri(); ?>/svg/facebook.svg" alt="Facebook Link"
-                        class="icon-youtube">
-
-                </a>
-            </li>
-            <li>
-                <a href="https://www.instagram.com/still_stayer" target="_blank" rel="noopener noreferrer">
-                    <img src="<?php echo get_template_directory_uri(); ?>/svg/instagram.svg" alt="Instagram Link"
-                        class="icon-instagram">
-                </a>
-            </li>
-            <li>
-                <a href="https://www.youtube.com/@stillstayer" target="_blank" rel="noopener noreferrer">
-                    <img src="<?php echo get_template_directory_uri(); ?>/svg/youtube.svg" alt="YouTube Link"
-                        class="icon-youtube">
-                </a>
-            </li>
-                        <li>
-                <a href="https://www.tiktok.com/@stillstayer" target="_blank" rel="noopener noreferrer">
-                    <img src="<?php echo get_template_directory_uri(); ?>/svg/tiktok.svg" alt="TikTok Link"
-                        class="icon-tiktok">
-                </a>
-            </li>
-            
-        </ul>
-    </div>
     <div class="footer-copy">
-        <p>&copy; <?php echo date("Y"); ?> Still Stayer. All Heart, No Brakes. All rights reserved.</p>
+        <p>&copy; <?php echo date("Y"); ?> Zack Morgenthaler. All rights reserved.</p>
     </div>
 </footer>
 </body>
--- header.php
+++ header.php
@@ -3,7 +3,6 @@
 
 <head>
   <meta charset="<?php bloginfo('charset'); ?>">
-  <meta name="description" content="Still Stayer's official website. Explore our music, shows, merch, and more.">
 
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
@@ -26,38 +25,3 @@
 </head>
 
 <body <?php body_class(); ?>>
-  <nav id="nav_main">
-    <div id="navbar">
-      <a href="/" id="nav_logo">
-        <img src="<?php echo get_template_directory_uri(); ?>/img/LOTUS FINAL blue.png" alt="Still Stayer Logo" />
-      </a>
-      <button id="nav_toggle" class="btn" onclick="handleNavClick()">
-        <span></span>
-        <span></span>
-        <span></span>
-      </button>
-    </div>
-    <ul id="nav_list">
-      <li>
-        <a href="/about/">About</a>
-      </li>
-      <li>
-        <a href="/listen/">Listen</a>
-      </li>
-      <li>
-        <a href="/shows/">Shows</a>
-      </li>
-      <li>
-        <a href="/merch/">Merch</a>
-      </li>
-      <li>
-        <a href="/gallery/">Gallery</a>
-      </li>
-      <li>
-        <a href="/blog/">Blog</a>
-      </li>
-      <li>
-        <a href="/contact/">Contact</a>
-      </li>
-    </ul>
-  </nav>

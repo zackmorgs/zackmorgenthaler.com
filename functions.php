848708f2863b3cf421235d4d1632af90ad264d99
zackmorgs <zackmorgenthaler@gmail.com>
2025-07-03 11:40:08 -0500

    updated functions

--- functions.php
+++ functions.php
@@ -1,7 +1,6 @@
 <?php
-function stillstayer_enqueue_styles() {
+function zackmorgenthaler_enqueue_styles(): void {
     wp_enqueue_style('main-style', get_stylesheet_uri());
 }
 
-add_action('wp_enqueue_scripts', 'stillstayer_enqueue_styles');
-
+add_action('wp_enqueue_scripts', 'zackmorgenthaler_enqueue_styles');

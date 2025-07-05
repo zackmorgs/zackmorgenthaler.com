fd073354ccfe579e8949903ae3d9629d14cc8a08
zackmorgs <zackmorgenthaler@gmail.com>
2025-07-05 11:07:03 -0500

    added favicon

--- header.php
+++ header.php
@@ -6,6 +6,10 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title><?php wp_title(); ?></title>
   <?php wp_head(); ?>
+  <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri(); ?>/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri(); ?>/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri(); ?>/favicon-16x16.png">
+  <link rel="manifest" href="<?php echo get_template_directory_uri(); ?>/site.webmanifest">
 </head>
 
 <body <?php body_class(); ?>>
